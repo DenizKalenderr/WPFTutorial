02321ab45951f3300b1f30bac297a7d4c292bf4a
DenizKalenderr <denzkalendr19@gmail.com>
2024-02-18 23:19:04 +0300

    Proje dosyası ekle.

---
+++ WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace WPFTutorial
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                //run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+
+            
+            running = !running;
+
+        }
+    }
+}
